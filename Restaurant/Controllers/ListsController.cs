b640e86d41a2668dacbe680a3b998d1643545924
Mohammed Al-yaseen <mouhamadalyasen3555@gmail.com>
2022-01-02 07:24:09 +0200

    Add list of products view & refactor category view

---
+++ Restaurant/Controllers/ListsController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Restaurant.Models;
+
+namespace Restaurant.Controllers
+{
+    public class ListsController : Controller
+    {
+        private readonly ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: Lists
+        public ActionResult Index()
+        {
+            return View(db.Products.ToList());
+        }
+    }
+}
